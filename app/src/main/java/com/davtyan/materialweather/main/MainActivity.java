d2defcf014d0b78773560fb6570c103bccfd1294
Edgar Davtyan <edgar.davtyan@outlook.com>
2017-02-09 22:58:04 +0200

    Make statusbar and toolbar transparent

--- app/src/main/java/com/davtyan/materialweather/main/MainActivity.java
+++ app/src/main/java/com/davtyan/materialweather/main/MainActivity.java
@@ -1,7 +1,10 @@
 package com.davtyan.materialweather.main;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -33,6 +36,13 @@ public class MainActivity extends AppCompatActivity implements MainMvp.View {
         MainFactory factory = app.getTodayWeatherFactory(this, this, "Kyiv");
         MainMvp.Presenter presenter = factory.getPresenter();
         presenter.onCreate();
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
     }
 
     @Override
