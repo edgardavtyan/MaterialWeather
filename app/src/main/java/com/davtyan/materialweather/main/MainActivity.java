1dbda7951ade2705cb7d2268bd1144be3b6ad724
Edgar Davtyan <edgar.davtyan@outlook.com>
2017-02-10 14:46:52 +0200

    Add background to status bar

--- app/src/main/java/com/davtyan/materialweather/main/MainActivity.java
+++ app/src/main/java/com/davtyan/materialweather/main/MainActivity.java
@@ -1,8 +1,8 @@
 package com.davtyan.materialweather.main;
 
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements MainMvp.View {
             getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.black_dim_strong));
         }
     }
 
