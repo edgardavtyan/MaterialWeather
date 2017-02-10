b6d51e450f9396ee8504032b915f5566eedfe047
Edgar Davtyan <edgar.davtyan@outlook.com>
2017-02-10 17:04:18 +0200

    Rename field

--- app/src/main/java/com/davtyan/materialweather/main/MainModel.java
+++ app/src/main/java/com/davtyan/materialweather/main/MainModel.java
@@ -4,20 +4,20 @@ import com.davtyan.materialweather.providers.darksky.DarkSkyWeatherProvider;
 
 public class MainModel implements MainMvp.Model {
     private final String location;
-    private final DarkSkyWeatherProvider darkSkyWeatherProvider;
+    private final DarkSkyWeatherProvider weatherProvider;
 
-    public MainModel(DarkSkyWeatherProvider darkSkyWeatherProvider, String location) {
-        this.darkSkyWeatherProvider = darkSkyWeatherProvider;
+    public MainModel(DarkSkyWeatherProvider weatherProvider, String location) {
+        this.weatherProvider = weatherProvider;
         this.location = location;
     }
 
     @Override
     public void getTodayWeather(TodayWeatherTask.Callback callback) {
-        if (darkSkyWeatherProvider.isCachedForecastAvailable()) {
-            TodayForecast forecast = darkSkyWeatherProvider.getForecastFromCache(location);
+        if (weatherProvider.isCachedForecastAvailable()) {
+            TodayForecast forecast = weatherProvider.getForecastFromCache(location);
             callback.onWeatherLoaded(forecast);
         } else {
-            new TodayWeatherTask(darkSkyWeatherProvider, callback).execute(location);
+            new TodayWeatherTask(weatherProvider, callback).execute(location);
         }
     }
 }
