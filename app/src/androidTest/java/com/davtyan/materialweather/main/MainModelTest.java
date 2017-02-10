8c83323fae866a6aa6608d3233ca7a5e9147aa55
Edgar Davtyan <edgar.davtyan@outlook.com>
2017-02-10 16:21:42 +0200

    Unit test MainModel

---
+++ app/src/androidTest/java/com/davtyan/materialweather/main/MainModelTest.java
@@ -0,0 +1,49 @@
+package com.davtyan.materialweather.main;
+
+import com.davtyan.materialweather.lib_test.BaseTest;
+import com.davtyan.materialweather.providers.darksky.DarkSkyWeatherProvider;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class MainModelTest extends BaseTest {
+    private static final String LOCATION = "Location";
+
+    private MainMvp.Model model;
+    private DarkSkyWeatherProvider weatherProvider;
+
+    @Override
+    public void beforeEach() {
+        super.beforeEach();
+        weatherProvider = mock(DarkSkyWeatherProvider.class);
+        model = new MainModel(weatherProvider, LOCATION);
+    }
+
+    @Test
+    public void getTodayWeather_getForecastFromCache() {
+        TodayForecast forecast = mock(TodayForecast.class);
+        when(weatherProvider.getForecastFromCache(LOCATION)).thenReturn(forecast);
+        when(weatherProvider.isCachedForecastAvailable()).thenReturn(true);
+
+        TodayWeatherTask.Callback callback = mock(TodayWeatherTask.Callback.class);
+        model.getTodayWeather(callback);
+
+        verify(callback).onWeatherLoaded(forecast);
+    }
+
+    @Test
+    public void getTodayWeather_getForecastFromProvider() {
+        TodayForecast forecast = mock(TodayForecast.class);
+        when(weatherProvider.getForecastForToday(LOCATION)).thenReturn(forecast);
+        when(weatherProvider.isCachedForecastAvailable()).thenReturn(false);
+
+        TodayWeatherTask.Callback callback = mock(TodayWeatherTask.Callback.class);
+        model.getTodayWeather(callback);
+
+        verify(callback, timeout(1000)).onWeatherLoaded(forecast);
+    }
+}
