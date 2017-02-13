7113c1a6ef548375c43eb9e7622b0fe146685f27
Edgar Davtyan <edgar.davtyan@outlook.com>
2017-02-13 14:31:09 +0200

    Use a library for geocoding

--- app/src/main/java/com/davtyan/materialweather/providers/Geocoding.java
+++ app/src/main/java/com/davtyan/materialweather/providers/Geocoding.java
@@ -1,28 +1,30 @@
 package com.davtyan.materialweather.providers;
 
 import android.content.Context;
-import android.location.Address;
-import android.location.Geocoder;
 
-import java.io.IOException;
+import com.doctoror.geocoder.Address;
+import com.doctoror.geocoder.Geocoder;
+import com.doctoror.geocoder.GeocoderException;
+
+import java.util.Locale;
 
 public class Geocoding {
     private final Geocoder geocoder;
 
     public Geocoding(Context context) {
-        geocoder = new Geocoder(context);
+        geocoder = new Geocoder(context, Locale.getDefault());
     }
 
     public LocationInfo getAddressFromLocation(String location) {
         try {
-            Address address = geocoder.getFromLocationName(location, 1).get(0);
+            Address address = geocoder.getFromLocationName(location, 1, true).get(0);
             LocationInfo locationInfo = new LocationInfo();
-            locationInfo.setLongitude(address.getLongitude());
-            locationInfo.setLatitude(address.getLatitude());
-            locationInfo.setCountry(address.getCountryName());
-            locationInfo.setAdminArea(address.getAdminArea());
+            locationInfo.setLongitude(address.getLocation().longitude);
+            locationInfo.setLatitude(address.getLocation().latitude);
+            locationInfo.setCountry(address.getCountry());
+            locationInfo.setAdminArea(address.getAdministrativeAreaLevel2());
             return locationInfo;
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
